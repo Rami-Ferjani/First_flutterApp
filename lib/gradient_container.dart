a986e92912d1047916d4967b3fe76557facfcb77
Rami-Ferjani <rami.ferjani3@gmail.com>
2023-09-11 17:24:41 +0100

    added text button

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -25,9 +25,24 @@ class GradientContainer extends StatelessWidget {
       ),
       child: Center(
           child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
           Image.asset('assets/dice-images/dice-2.png', width: 250),
-          TextButton(onPressed: rollDice, child: const Text('Roll Dice'))
+          const SizedBox(
+            height: 20,
+          ),
+          TextButton(
+            onPressed: rollDice,
+            style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                ),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                )),
+            child: const Text('Roll Dice'),
+          )
         ],
       )),
     );
